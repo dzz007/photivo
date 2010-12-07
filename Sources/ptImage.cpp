eec2aa76f8a789eac61f9a1fd487ad3af6093135
mike <mike@localhost>
2010-12-07 23:05:23 +0100

    Fixed bug in gimp export

--- Sources/ptImage.cpp
+++ Sources/ptImage.cpp
@@ -286,7 +286,7 @@ ptImage* ptImage::lcmsRGBToRGB(cmsHPROFILE OutProfile,
     Transform =
       cmsCreateTransform(InProfile,
                          TYPE_RGB_16,
-                         PreviewColorProfile,
+                         OutProfile,
                          TYPE_RGB_16,
                          Intent,
                          cmsFLAGS_HIGHRESPRECALC | cmsFLAGS_BLACKPOINTCOMPENSATION);
@@ -294,7 +294,7 @@ ptImage* ptImage::lcmsRGBToRGB(cmsHPROFILE OutProfile,
     Transform =
       cmsCreateTransform(InProfile,
                          TYPE_RGB_16,
-                         PreviewColorProfile,
+                         OutProfile,
                          TYPE_RGB_16,
                          Intent,
                          cmsFLAGS_NOOPTIMIZE | cmsFLAGS_BLACKPOINTCOMPENSATION);
