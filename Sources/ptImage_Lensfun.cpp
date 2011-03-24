5d91e098a3e313c0675f4b283953e795b4472cfa
Brother John <Brother John@localhost>
2011-03-24 22:44:55 +0100

    Bicubic interpolation (same as for the perspective tool)

--- Sources/ptImage_Lensfun.cpp
+++ Sources/ptImage_Lensfun.cpp
@@ -23,7 +23,13 @@
 #include <lensfun.h>
 #include "ptImage.h"
 
-ptImage* ptImage::Lensfun(const int LfunActions, const lfModifier* LfunData, float* TransformedCoords) {
+typedef float Tfloat;   // needed by the interpolation code
+
+ptImage* ptImage::Lensfun(const int LfunActions, const lfModifier* LfunData) {
+  struct Coords {
+    float x;
+    float y;
+  };
 
   // Stage 2: Vignetting.
   if ((LfunActions & LF_MODIFY_VIGNETTING) ||
@@ -31,27 +37,67 @@ ptImage* ptImage::Lensfun(const int LfunActions, const lfModifier* LfunData, flo
       (LfunActions == LF_MODIFY_ALL) )
   {
     LfunData->ApplyColorModification(m_Image, 0.0, 0.0, m_Width, m_Height,
-                                     LF_CR_4(RED, GREEN, BLUE, GREEN),  // TODO BJ: Taken from Jos’s old code. I have
-                                     m_Width * 4 * sizeof(uint16_t));   // no idea why it has to be LF_CRF_4
+                                     LF_CR_3(RED, GREEN, BLUE),
+                                     m_Width * 3 * sizeof(uint16_t));
   }
 
-  // Stage 1+3: CA, lens Geometry/distortion
-  if (((LfunActions & LF_MODIFY_TCA) &&
-      (LfunActions & (LF_MODIFY_DISTORTION | LF_MODIFY_GEOMETRY))) ||
+  // Stage 1 and/or 3: CA, lens Geometry/distortion
+  // Processing row by row to avoid *huge* memory requirements
+  if (((LfunActions & LF_MODIFY_TCA) || (LfunActions & LF_MODIFY_DISTORTION) || (LfunActions & LF_MODIFY_GEOMETRY)) ||
       (LfunActions == LF_MODIFY_ALL) )
   {
-    TransformedCoords = (float*) CALLOC((uint32_t)m_Width * m_Height * 4 * 2, sizeof(float));
-    LfunData->ApplySubpixelGeometryDistortion(0.0, 0.0, m_Width, m_Height, TransformedCoords);
+printf("!!!!!!START\n");
+    Coords NewCoords[m_Width];  //CALLOC((uint32_t)m_Width * 2, sizeof(float));
+    memset(NewCoords, 0, sizeof(Coords)*m_Width);
+    uint16_t (*TempImage)[3] = (uint16_t (*)[3]) CALLOC((int32_t)m_Width * m_Height, sizeof(*TempImage));
 
-  // Stage 1 only: CA
-  } else if (LfunActions & LF_MODIFY_TCA) {
-    TransformedCoords = (float*) CALLOC((uint32_t)m_Width * m_Height * 4 * 2, sizeof(float));
-    LfunData->ApplySubpixelDistortion(0.0, 0.0, m_Width, m_Height, TransformedCoords);
+    for (uint16_t row = 0; row < m_Height; row++) {
+      int32_t Temp = row * m_Width;
+printf("!!!!!!MARKER\n");
+      LfunData->ApplySubpixelGeometryDistortion(0.0, row, m_Width, 1, (float*)&NewCoords);
+printf("!!!!!!MARKER222222222\n");
 
-  // Stage 3 only: lens geometry/distortion
-  } else if (LfunActions & (LF_MODIFY_DISTORTION | LF_MODIFY_GEOMETRY)) {
-    TransformedCoords = (float*) CALLOC((uint32_t)m_Width * m_Height * 4 * 2, sizeof(float));
-    LfunData->ApplyGeometryDistortion(0.0, 0.0, m_Width, m_Height, TransformedCoords);
+      // Bicubic interpolation
+      for (uint16_t col = 0; col < m_Width; col++) {
+        printf("!!!!!!MARKER333333333\n");
+        printf("!!!%u\n", (unsigned int)sizeof(NewCoords));
+        if (NewCoords[col].x < 0 || NewCoords[col].x > m_Width-1 || NewCoords[col].y < 0 || NewCoords[col].y > m_Height-1) {
+          for (short c = 0; c < 3; c++) {
+            TempImage[Temp+col][c] = 0;
+          }
+          continue;
+        }
+        const int32_t
+          x = (int32_t)NewCoords[col].x,
+          y = (int32_t)NewCoords[col].y;
+        const float
+          dx = NewCoords[col].x - x,
+          dy = NewCoords[col].y - y;
+        const int32_t
+          px = x-1<0?0:x-1, nx = dx>0?x+1:x, ax = x+2>=m_Width?m_Width-1:x+2,
+          py = y-1<0?0:y-1, ny = dy>0?y+1:y, ay = y+2>=m_Height?m_Height-1:y+2;
+        const int32_t
+          pyw = py * m_Width, yw = y * m_Width,
+          nyw = ny * m_Width, ayw = ay * m_Width;
+        for (short c = 0; c < 3; c++) {
+          const Tfloat
+            Ipp = m_Image[px+pyw][c], Icp = m_Image[x+pyw][c], Inp = m_Image[nx+pyw][c], Iap = m_Image[ax+pyw][c],
+            Ip = Icp + 0.5f*(dx*(-Ipp+Inp) + dx*dx*(2*Ipp-5*Icp+4*Inp-Iap) + dx*dx*dx*(-Ipp+3*Icp-3*Inp+Iap)),
+            Ipc = m_Image[px+yw][c], Icc = m_Image[x+yw][c], Inc = m_Image[nx+yw][c], Iac = m_Image[ax+yw][c],
+            Ic = Icc + 0.5f*(dx*(-Ipc+Inc) + dx*dx*(2*Ipc-5*Icc+4*Inc-Iac) + dx*dx*dx*(-Ipc+3*Icc-3*Inc+Iac)),
+            Ipn = m_Image[px+nyw][c], Icn = m_Image[x+nyw][c], Inn = m_Image[nx+nyw][c], Ian = m_Image[ax+nyw][c],
+            In = Icn + 0.5f*(dx*(-Ipn+Inn) + dx*dx*(2*Ipn-5*Icn+4*Inn-Ian) + dx*dx*dx*(-Ipn+3*Icn-3*Inn+Ian)),
+            Ipa = m_Image[px+ayw][c], Ica = m_Image[x+ayw][c], Ina = m_Image[nx+ayw][c], Iaa = m_Image[ax+ayw][c],
+            Ia = Ica + 0.5f*(dx*(-Ipa+Ina) + dx*dx*(2*Ipa-5*Ica+4*Ina-Iaa) + dx*dx*dx*(-Ipa+3*Ica-3*Ina+Iaa));
+          const Tfloat val = Ic + 0.5f*(dy*(-Ip+In) + dy*dy*(2*Ip-5*Ic+4*In-Ia) + dy*dy*dy*(-Ip+3*Ic-3*In+Ia));
+          TempImage[Temp+col][c] = CLIP((int32_t) val);
+        }
+      }
+    }
+
+    free(&NewCoords);
+    FREE(m_Image);
+    m_Image = TempImage;
   }
 
   return this;
