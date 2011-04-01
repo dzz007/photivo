8d62acb4f85cb609d01220f3f767fa45c82c14f6
mike <mike@localhost>
2011-04-01 22:30:32 +0200

    Energy functions for liquid rescale

---
+++ Sources/ptRGBTemperature.h
@@ -0,0 +1,31 @@
+/*******************************************************************************
+**
+** Photivo
+**
+** Copyright (C) 2008 Jos De Laender <jos.de_laender@telenet.be>
+**
+** This file is part of Photivo.
+**
+** Photivo is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License version 3
+** as published by the Free Software Foundation.
+**
+** Photivo is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Photivo.  If not, see <http:**www.gnu.org/licenses/>.
+**
+*******************************************************************************/
+
+#ifndef DLRGBTEMPERATURE_H
+#define DLRGBTEMPERATURE_H
+
+void TemperatureToRGB(int Temperature, double RGB[3]);
+void RGBToTemperature(double RGB[3], int *Temperature, double *Green);
+
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
