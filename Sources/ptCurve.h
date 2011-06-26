9f35506a523d947087e021f47723117d881b1c7d
salsergey <salsergey@localhost>
2011-06-26 21:09:14 +0700

    Russian translation updated

---
+++ Sources/ptCurve.h
@@ -0,0 +1,165 @@
+/*******************************************************************************
+**
+** Photivo
+**
+** Copyright (C) 2008 Jos De Laender <jos.de_laender@telenet.be>
+** Copyright (C) 2010 Michael Munzert <mail@mm-log.com>
+**
+** This file is part of Photivo.
+**
+** Photivo is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License version 3
+** as published by the Free Software Foundation.
+**
+** Photivo is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Photivo.  If not, see <http://www.gnu.org/licenses/>.
+**
+*******************************************************************************/
+
+#ifndef DLCURVE_H
+#define DLCURVE_H
+
+#include "ptDefines.h"
+#include "ptConstants.h"
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// The class ptCurve is the abstraction of a curve to be used.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+// A forward declaration to the image class.
+class ptImage;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// 'gamma' functions operating in the (0,0)(1,1) box.
+//
+// GammaSRGB is the correct one for sRGB encoding.
+// GammaBT709 is the BT709 one , used by dcraw.
+// GammaPure22 is a pure 2.2 gamma curve.
+// GammaTool is the gamma function as used in ufraw.
+// DeltaGammaTool is the gamma function as used in ufraw, but
+//    sRGB 'subtracted' as it is added afterwards as standard part of the flow.
+// InverseGammaSRGB is the inverse for sRGB encoding.
+//
+// Args is sometimes dummy but required for matching general signature of
+// SetCurveFromFunction.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+double GammaBT709(double r, double Dummy1, double Dummy2);
+double GammaSRGB(double r, double Dummy1, double Dummy2);
+double GammaPure22(double r, double Dummy1, double Dummy2);
+double GammaTool(double r, double Gamma, double Linearity);
+double DeltaGammaTool(double r, double Gamma, double Linearity);
+double InverseGammaSRGB(double r, double Dummy1, double Dummy2);
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// 'Sigmoidal' function operating in the (0,0)(1,1) box.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+double Sigmoidal(double r, double Threshold, double Contrast);
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Class containing a curve and its operations.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+class ptCurve {
+public :
+
+short  m_IntendedChannel; // RGB|R|G|B|L
+short  m_Type;            // Anchor or Full.
+
+// 0x10000 samples of 0..0xffff values.
+uint16_t m_Curve[0x10000];
+
+// Nr of anchors when described in terms of anchors.
+// The anchors in the (0,0)-(1,1) box.
+short  m_NrAnchors;
+double m_XAnchor[ptMaxAnchors];
+double m_YAnchor[ptMaxAnchors];
+
+// Interpolation Type
+short  m_IntType;
+
+// Read the anchors from a simple file in the format X0 Y0 \n X1 Y1 ...
+// Returns 0 on success.
+short ReadAnchors(const char *FileName);
+
+// More complete reading and writing function (compatible).
+// Header is a free text that is inserted as comment to describe
+// the curve.
+short WriteCurve(const char* FileName,const char *Header = NULL);
+short ReadCurve(const char* FileName);
+
+// Constructor
+ptCurve(const short Channel = 0);
+
+// Destructor
+~ptCurve();
+
+// Set Curve from Curve
+short Set(ptCurve *Curve);
+
+// Sets a curve , via splines , from anchors.
+// Returns 0 on success.
+short SetCurveFromAnchors();
+
+// Sets a straight line with Anchors.
+short SetNullCurve(const short Channel);
+
+// Sets a curve from a mathematical function over the (0,0)..(1,1) box.
+//   The function is passed as parameter Function who's first argument
+//     will be the x (0..1) of the function.
+//   Optional arguments in ... are passed to the Args part of Function.
+// Returns 0 on success.
+short SetCurveFromFunction(
+                         double(*Function)(double r, double Arg1, double Arg2),
+                         double Arg1,
+                         double Arg2);
+
+// Apply a curve on a curve (=multiplication).
+// Multiplication of curves is not commutative.
+// AfterThis (read 'after this') = 1 applies the Curve argument
+//   on the result of 'this' curve.
+// AfterThis = 0 applies the Curve argument before applying 'this' curve.
+ptCurve* ApplyCurve(const ptCurve  *Curve,
+                    const short    AfterThis);
+
+// Dumps the curve data to a file.
+// Scale defines by what Y is divided and over what amount X is sampled
+// Returns 0 on success.
+short DumpData(const char* FileName,
+               const short Scale=0x100);
+
+
+// Spline functions.
+// If needed see details in the code, but you normally would not use those.
+double *d3_np_fs( int n, double a[], double b[] );
+double *spline_cubic_set (int n, double t[], double y[], int ibcbeg,
+                          double ybcbeg, int ibcend, double ybcend );
+double spline_cubic_val (int n, double t[], double tval, double y[],
+                         double ypp[], double *ypval, double *yppval );
+};
+
+// Some program wide defined curves
+extern ptCurve*  RGBGammaCurve;
+extern ptCurve*  RGBContrastCurve;
+extern ptCurve*  ExposureCurve;
+extern ptCurve*  ContrastCurve;
+// RGB,R,G,B,L,a,b,Base
+extern ptCurve*  Curve[16];
+
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
---
+++ Sources/ptImageMagick.cpp
@@ -0,0 +1,372 @@
+/*******************************************************************************
+**
+** Photivo
+**
+** Copyright (C) 2008,2009 Jos De Laender <jos.de_laender@telenet.be>
+** Copyright (C) 2009,2010 Michael Munzert <mail@mm-log.com>
+**
+** This file is part of Photivo.
+**
+** Photivo is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License version 3
+** as published by the Free Software Foundation.
+**
+** Photivo is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Photivo.  If not, see <http://www.gnu.org/licenses/>.
+**
+*******************************************************************************/
+
+#include <QString>
+#include <QMessageBox>
+
+#include "ptImageMagick.h"
+#include "ptConstants.h"
+#include "ptError.h"
+#include "ptSettings.h"
+
+using namespace Magick;
+
+// Lut
+extern float ToFloatTable[0x10000];
+/*
+void ptIMResize(ptImage* Image, uint16_t Size, const short Filter) {
+
+  uint16_t Width  = Image->m_Width;
+  uint16_t Height = Image->m_Height;
+
+  Magick::Image image(Width,Height,"RGB",ShortPixel,Image->m_Image);
+  FREE(Image->m_Image);
+
+  switch (Filter) {
+    case ptIMFilter_Point:
+      image.filterType(PointFilter);
+      break;
+    case ptIMFilter_Box:
+      image.filterType(BoxFilter);
+      break;
+    case ptIMFilter_Triangle:
+      image.filterType(TriangleFilter);
+      break;
+    case ptIMFilter_Hermite:
+      image.filterType(HermiteFilter);
+      break;
+    case ptIMFilter_Hanning:
+      image.filterType(HanningFilter);
+      break;
+    case ptIMFilter_Hamming:
+      image.filterType(HammingFilter);
+      break;
+    case ptIMFilter_Blackman:
+      image.filterType(BlackmanFilter);
+      break;
+    case ptIMFilter_Gaussian:
+      image.filterType(GaussianFilter);
+      break;
+    case ptIMFilter_Quadratic:
+      image.filterType(QuadraticFilter);
+      break;
+    case ptIMFilter_Cubic:
+      image.filterType(CubicFilter);
+      break;
+    case ptIMFilter_Catrom:
+      image.filterType(CatromFilter);
+      break;
+    case ptIMFilter_Mitchell:
+      image.filterType(MitchellFilter);
+      break;
+    case ptIMFilter_Lanczos:
+      image.filterType(LanczosFilter);
+      break;
+    //~ case ptIMFilter_Bessel:
+      //~ image.filterType(BesselFilter);
+      //~ break;
+    //~ case ptIMFilter_Sinc:
+      //~ image.filterType(SincFilter);
+      //~ break;
+    default:
+      assert(0);
+  }
+
+  QString TempString = QString::number(Size);
+  if (Width <= Height)  {
+    TempString.prepend("x");
+  }
+
+  image.zoom(TempString.toStdString());
+  image.modifyImage();
+
+  Image->m_Width  = image.columns();
+  Image->m_Height = image.rows();
+  Image->m_Image = (uint16_t (*)[3]) CALLOC(Image->m_Width*Image->m_Height,sizeof(*Image->m_Image));
+  ptMemoryError(Image->m_Image,__FILE__,__LINE__);
+
+  image.write(0,0,image.columns(),image.rows(),"RGB",ShortPixel,Image->m_Image);
+}
+*/
+
+/*
+void ptIMRotate(ptImage* Image, const double Angle) {
+
+  uint16_t Width  = Image->m_Width;
+  uint16_t Height = Image->m_Height;
+
+  double WP = 0xFFFF;
+  double invWP = 1/WP;
+
+  Magick::Image image;
+  image.size(Geometry(Width, Height));
+  image.type(TrueColorType);
+  image.magick( "RGB" );
+  image.depth( 16 );
+
+  image.modifyImage();
+
+  Magick::PixelPacket *Pixels;
+  const Magick::PixelPacket *Pixels2;
+  Magick::ColorRGB Color;
+
+  for (uint16_t Row=0; Row < Height; Row++) {
+    Pixels = image.setPixels(0, Row, Width, 1);
+    for (uint16_t Col=0; Col < Width; Col++) {
+      Color.red(ToFloatTable[Image->m_Image[Row*Width+Col][0]]);
+      Color.green(ToFloatTable[Image->m_Image[Row*Width+Col][1]]);
+      Color.blue(ToFloatTable[Image->m_Image[Row*Width+Col][2]]);
+      *Pixels++ = Color;
+    }
+    image.syncPixels();
+  }
+
+  image.modifyImage();
+  image.virtualPixelMethod(BlackVirtualPixelMethod);
+  image.distort(ScaleRotateTranslateDistortion,1,&Angle,1);
+  uint16_t NewWidth = image.columns();
+  uint16_t NewHeight = image.rows();
+  image.modifyImage();
+
+  FREE(Image->m_Image);
+  Image->m_Width  = NewWidth;
+  Image->m_Height = NewHeight;
+  Image->m_Image = (uint16_t (*)[3]) CALLOC(Image->m_Width*Image->m_Height,sizeof(*Image->m_Image));
+  ptMemoryError(Image->m_Image,__FILE__,__LINE__);
+
+  for (uint16_t Row=0; Row < NewHeight; Row++) {
+    Pixels2 = image.getConstPixels(0, Row, NewWidth, 1);
+    for (uint16_t Col=0; Col < NewWidth; Col++) {
+      Color = (*(Pixels2 + Col));
+      Image->m_Image[Row*NewWidth+Col][0] = CLIP((int32_t) (Color.red()*WP));
+      Image->m_Image[Row*NewWidth+Col][1] = CLIP((int32_t) (Color.green()*WP));
+      Image->m_Image[Row*NewWidth+Col][2] = CLIP((int32_t) (Color.blue()*WP));
+    }
+  }
+
+}
+*/
+
+/*
+void ptIMReadJpg(ptImage* Image, const char* FileName) {
+
+  double WP = 0xFFFF;
+
+  Magick::Image image;
+  const Magick::PixelPacket *Pixels2;
+  Magick::ColorRGB Color;
+
+  image.read(FileName);
+  image.type(TrueColorType);
+  image.magick( "RGB" );
+  image.depth( 16 );
+
+  image.modifyImage();
+
+  uint16_t NewWidth = image.columns();
+  uint16_t NewHeight = image.rows();
+  image.modifyImage();
+
+  FREE(Image->m_Image);
+  Image->m_Width  = NewWidth;
+  Image->m_Height = NewHeight;
+  Image->m_Image = (uint16_t (*)[3]) CALLOC(Image->m_Width*Image->m_Height,sizeof(*Image->m_Image));
+  ptMemoryError(Image->m_Image,__FILE__,__LINE__);
+
+  for (uint16_t Row=0; Row < NewHeight; Row++) {
+    Pixels2 = image.getConstPixels(0, Row, NewWidth, 1);
+    for (uint16_t Col=0; Col < NewWidth; Col++) {
+      Color = (*(Pixels2 + Col));
+      Image->m_Image[Row*NewWidth+Col][0] = CLIP((int32_t) (Color.red()*WP));
+      Image->m_Image[Row*NewWidth+Col][1] = CLIP((int32_t) (Color.green()*WP));
+     Image->m_Image[Row*NewWidth+Col][2] = CLIP((int32_t) (Color.blue()*WP));
+    }
+  }
+
+}
+*/
+
+/*
+void ptIMUnsharp(ptImage* Image, const double Radius, const double Amount, const double Threshold) {
+
+  assert (Image->m_ColorSpace == ptSpace_Lab);
+
+  uint16_t Width  = Image->m_Width;
+  uint16_t Height = Image->m_Height;
+
+  Magick::Image image(Width,Height,"RGB",ShortPixel,Image->m_Image);
+
+  double Sigma = Radius;
+  image.unsharpmaskChannel(RedChannel,0,Sigma,Amount,Threshold);
+  image.modifyImage();
+
+  image.write(0,0,Width,Height,"RGB",ShortPixel,Image->m_Image);
+}
+*/
+
+/*
+void ptIMBlur(ptImage* Image, const double Radius) {
+
+  uint16_t Width  = Image->m_Width;
+  uint16_t Height = Image->m_Height;
+
+  Magick::Image image(Width,Height,"RGB",ShortPixel,Image->m_Image);
+
+  image.blur(Radius);
+  image.modifyImage();
+
+  image.write(0,0,Width,Height,"RGB",ShortPixel,Image->m_Image);
+}
+*/
+
+/*
+void ptIMNormalize(ptImage* Image, const double Opacity) {
+
+  uint16_t Width  = Image->m_Width;
+  uint16_t Height = Image->m_Height;
+
+  double WP = 0xFFFF;
+  double invWP = 1/WP;
+
+  Magick::Image image;
+  image.size(Geometry(Width, Height));
+  image.type(TrueColorType);
+  image.magick( "RGB" );
+  image.depth( 16 );
+
+  image.modifyImage();
+
+  Magick::PixelPacket *Pixels;
+  const Magick::PixelPacket *Pixels2;
+  Magick::ColorRGB Color;
+#pragma omp parallel for default(shared) private(Pixels, Color)
+  for (uint16_t Row=0; Row < Height; Row++) {
+  Pixels = image.setPixels(0, Row, Width, 1);
+    for (uint16_t Col=0; Col < Width; Col++) {
+      Color.red(invWP * Image->m_Image[Row*Width+Col][0]);
+      Color.green(invWP * Image->m_Image[Row*Width+Col][1]);
+    Color.blue(invWP * Image->m_Image[Row*Width+Col][2]);
+    *Pixels++ = Color;
+    }
+    image.syncPixels();
+  }
+
+  image.modifyImage();
+  image.normalize();
+
+  image.modifyImage();
+
+#pragma omp parallel for default(shared) private(Pixels2, Color)
+  for (uint16_t Row=0; Row < Height; Row++) {
+    Pixels2 = image.getConstPixels(0, Row, Width, 1);
+    for (uint16_t Col=0; Col < Width; Col++) {
+    Color = (*(Pixels2 + Col));
+    Image->m_Image[Row*Width+Col][0] = CLIP((int32_t)
+                                            ((1-Opacity)*Image->m_Image[Row*Width+Col][0]
+                                             + Opacity*Color.red()*WP));
+    Image->m_Image[Row*Width+Col][1] = CLIP((int32_t)
+                                            ((1-Opacity)*Image->m_Image[Row*Width+Col][1]
+                                             + Opacity*Color.green()*WP));
+    Image->m_Image[Row*Width+Col][2] = CLIP((int32_t)
+                                            ((1-Opacity)*Image->m_Image[Row*Width+Col][2]
+                                             + Opacity*Color.blue()*WP));
+    }
+  }
+
+}
+*/
+
+/*
+void IM_WriteImage(ptImage* Image,
+                   const char* FileName,
+                   const short Format,
+                   const int Quality,
+                   const char* ColorProfileFileName,
+                   const int Intent) {
+
+  uint16_t Width  = Image->m_Width;
+  uint16_t Height = Image->m_Height;
+
+  Magick::Image image(Width,Height,"RGB",ShortPixel,Image->m_Image);
+
+  // Color profile
+  // Color profile in PNG is not supported, due to limitations in imagemagick
+
+  FILE* pFile = fopen (ColorProfileFileName, "rb" );
+  if (pFile==NULL) {
+    ptLogError(ptError_FileOpen,FileName);
+    exit(EXIT_FAILURE);
+  }
+  fseek (pFile , 0 , SEEK_END);
+  long lSize = ftell (pFile);
+  rewind (pFile);
+
+  uint8_t* pchBuffer = (uint8_t*) CALLOC(lSize,sizeof(uint8_t));
+  ptMemoryError(pchBuffer,__FILE__,__LINE__);
+
+  size_t RV = fread (pchBuffer, 1, lSize, pFile);
+  if (RV != (size_t) lSize) {
+    ptLogError(ptError_FileOpen,FileName);
+    exit(EXIT_FAILURE);
+  }
+  Magick::Blob ICCProfile(pchBuffer, lSize);
+  image.profile("icc", ICCProfile);
+
+  FCLOSE (pFile);
+  FREE (pchBuffer);
+
+  // Redering intent
+  switch(Intent) {
+    case 0:
+      image.renderingIntent(PerceptualIntent);
+      break;
+    case 1:
+      image.renderingIntent(RelativeIntent);
+      break;
+    case 2:
+      image.renderingIntent(SaturationIntent);
+      break;
+    case 3:
+      image.renderingIntent(AbsoluteIntent);
+      break;
+    default:
+      assert(0);
+  }
+
+  // Depth
+  if ((Format==ptSaveFormat_TIFF8) ||
+      (Format==ptSaveFormat_PPM8) ||
+      (Format==ptSaveFormat_JPEG) ||
+      (Format==ptSaveFormat_PNG)) image.depth( 8 );
+
+  // Quality
+  if (Format==ptSaveFormat_PNG) image.quality(floor((double) Quality/10.0)*10+5);
+  else image.quality(Quality);
+
+  // Compression
+  if ((Format==ptSaveFormat_TIFF8) ||
+      (Format==ptSaveFormat_TIFF16)) image.compressType(LZWCompression);
+
+  image.write(FileName);
+}
+*/
