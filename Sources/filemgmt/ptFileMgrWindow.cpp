9ac2b8befa1fdf3059660a5149be3a488d074ded
Brother John <Brother John@localhost>
2011-10-04 19:55:40 +0200

    Rudimentary thumbnail display

--- Sources/filemgmt/ptFileMgrWindow.cpp
+++ Sources/filemgmt/ptFileMgrWindow.cpp
@@ -23,9 +23,9 @@
 
 #include <QFileSystemModel>
 
+#include "ptFileMgrWindow.h"
 #include "../ptDefines.h"
 #include "../ptSettings.h"
-#include "ptFileMgrWindow.h"
 
 extern ptSettings* Settings;
 
@@ -39,6 +39,7 @@ ptFileMgrWindow::ptFileMgrWindow(QWidget *parent)
 
   // We create our data module
   m_DataModel = ptFileMgrDM::GetInstance();
+  m_DataModel->setThumbSize(150);   //TODO: only temporarily hardcoded
   QFileSystemModel* fsmodel = qobject_cast<QFileSystemModel*>(m_DataModel->treeModel());
 
   DirTree->setModel(fsmodel);
@@ -48,11 +49,18 @@ ptFileMgrWindow::ptFileMgrWindow(QWidget *parent)
   DirTree->setColumnHidden(3, true);
   connect(DirTree, SIGNAL(clicked(QModelIndex)), this, SLOT(changeTreeDir(QModelIndex)));
 
+  DirTree->setFixedWidth(300);  //TODO: temporary
+
   // Setup the graphics scene
-  m_FilesScene = new QGraphicsScene;
+  m_FilesScene = new QGraphicsScene(0, 0, 0, 0, parent);
   FilesView->setScene(m_FilesScene);
   connect(m_DataModel->thumbnailer(), SIGNAL(newThumbsNotify()),
           this, SLOT(fetchNewThumbs()));
+
+  m_StatusOverlay = new ptReportOverlay(FilesView, "Reading thumbnails",
+                                        QColor(255,75,75), QColor(255,190,190),
+                                        0, Qt::AlignLeft, 20);
+
 }
 
 //==============================================================================
@@ -62,6 +70,7 @@ ptFileMgrWindow::~ptFileMgrWindow() {
       qobject_cast<QFileSystemModel*>(m_DataModel->treeModel())->filePath(DirTree->currentIndex()) );
 
   ptFileMgrDM::DestroyInstance();
+  DelAndNull(m_StatusOverlay);
 }
 
 //==============================================================================
@@ -70,22 +79,44 @@ void ptFileMgrWindow::changeTreeDir(const QModelIndex& index) {
   m_DataModel->StopThumbnailer();
   m_FilesScene->clear();
   m_DataModel->thumbQueue()->clear();
+  m_StatusOverlay->exec();
   m_DataModel->StartThumbnailer(DirTree->currentIndex());
 }
 
 //==============================================================================
 
 void ptFileMgrWindow::fetchNewThumbs() {
+  m_StatusOverlay->stop();
   if (m_DataModel->thumbQueue()->isEmpty()) {
     return;
   }
 
-  for (int i = 0; i < 4; i++) {
-    m_FilesScene->addItem(m_DataModel->thumbQueue()->dequeue());
+//  for (int i = 0; i < 4; i++) {
+//    m_FilesScene->addItem(m_DataModel->thumbQueue()->dequeue());
 
-    if (m_DataModel->thumbQueue()->isEmpty()) {
-      break;
+//    if (m_DataModel->thumbQueue()->isEmpty()) {
+//      break;
+//    }
+//  }
+
+  //m_FilesScene->setSceneRect(0, 0, FilesView->width(), FilesView->height());
+  int Row = -1;
+  int Col = 0;
+  int CellSize = m_DataModel->thumbSize() + 10;
+  int PicsInCol = (FilesView->height() / CellSize) - 1;
+
+  while (!m_DataModel->thumbQueue()->isEmpty()) {
+    QGraphicsItemGroup* ig = m_DataModel->thumbQueue()->dequeue();
+
+    if (Row >= PicsInCol) {
+      Row = 0;
+      Col++;
+    } else {
+      Row++;
     }
+
+    ig->setPos(Col * CellSize, Row * CellSize);
+    m_FilesScene->addItem(ig);
   }
 }
 
