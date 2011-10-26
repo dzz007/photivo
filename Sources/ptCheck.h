7348ad521107fff4aa070d7e885425903e3e2242
Brother John <Brother John@localhost>
2011-10-27 01:54:20 +0200

    "Link" mouse cursor for the toolbox help icon. Toolbox help icon and slow-filter icon are now right-justified and only appear on unfolded toolboxes. Reduces UI clutter a lot.

---
+++ Sources/ptCheck.h
@@ -0,0 +1,74 @@
+/*******************************************************************************
+**
+** Photivo
+**
+** Copyright (C) 2008 Jos De Laender <jos.de_laender@telenet.be>
+** Copyright (C) 2010 Michael Munzert <mail@mm-log.com>
+**
+** This file is part of Photivo.
+**
+** Photivo is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License version 3
+** as published by the Free Software Foundation.
+**
+** Photivo is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Photivo.  If not, see <http://www.gnu.org/licenses/>.
+**
+*******************************************************************************/
+
+#ifndef DLCHECK_H
+#define DLCHECK_H
+
+#include <QtGui>
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// ptCheck is a object showing a checkbox
+//
+////////////////////////////////////////////////////////////////////////////////
+
+class ptCheck : public QObject {
+
+Q_OBJECT
+
+public :
+
+ptCheck(const QWidget* MainWindow,
+        const QString  ObjectName,
+        const QString  ParentName,
+        const QVariant Default,
+        const QString  Label,
+        const QString  ToolTip);
+// Destructor.
+~ptCheck();
+
+void SetValue(const QVariant Value, const short BlockSignal = 1);
+void SetEnabled(const short Enabled);
+void Show(const short Show);
+void Reset();
+QString GetName() {return m_SettingsName;}
+
+protected:
+
+private slots:
+void OnValueChanged(int Value);
+
+signals :
+void valueChanged(QVariant Value);
+
+private:
+QWidget*   m_Parent;
+QVariant   m_DefaultValue;
+QVariant   m_Value;
+QCheckBox* m_CheckBox;
+QString    m_SettingsName;
+};
+
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
---
+++ Sources/ptReportOverlay.h
@@ -0,0 +1,93 @@
+/*******************************************************************************
+**
+** Photivo
+**
+** Copyright (C) 2009-2011 Michael Munzert <mail@mm-log.com>
+** Copyright (C) 2011 Bernd Schoeler <brjohn@brother-john.net>
+**
+** This file is part of Photivo.
+**
+** Photivo is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License version 3
+** as published by the Free Software Foundation.
+**
+** Photivo is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Photivo.  If not, see <http://www.gnu.org/licenses/>.
+**
+*******************************************************************************/
+/**
+** Displays the status overlays in the view window.
+**
+** - The overlay is displayed for duration() milliseconds. If you set the
+**   duration to 0 the overlay is displayed until you call stop().
+** - Foreground colours is for the text and wiget border. Background colour
+**/
+
+#ifndef PTREPORTOVERLAY_H
+#define PTREPORTOVERLAY_H
+
+#include <QTimer>
+#include <QLabel>
+
+
+///////////////////////////////////////////////////////////////////////////
+//
+// class ptViewWindow
+//
+///////////////////////////////////////////////////////////////////////////
+
+class ptReportOverlay : public QLabel
+{
+  Q_OBJECT
+
+///////////////////////////////////////////////////////////////////////////
+//
+// PUBLIC members
+//
+///////////////////////////////////////////////////////////////////////////
+public:
+  explicit ptReportOverlay(QWidget* parent,
+                           const QString& text,
+                           const QColor& color,
+                           const QColor& backgroundColor,
+                           const int duration,
+                           const Qt::Alignment pos,
+                           const int padding);
+  ~ptReportOverlay();
+
+  inline int duration() { return m_Timer->interval(); }
+  void exec(const QString& newText = "");
+  void setColors(const QColor& color, const QColor& backgroundColor);
+  inline void setDuration(const int msec) { m_Timer->setInterval(msec); }
+  void stop();
+
+
+///////////////////////////////////////////////////////////////////////////
+//
+// PRIVATE members
+//
+///////////////////////////////////////////////////////////////////////////
+private:
+  int m_Padding;
+  Qt::Alignment m_Position;  // only left or right for now
+  QTimer* m_Timer;
+  QWidget* m_Parent;
+
+  void UpdatePosition();
+
+
+///////////////////////////////////////////////////////////////////////////
+//
+// PRIVATE slots
+//
+///////////////////////////////////////////////////////////////////////////
+private slots:
+  void TimerExpired();
+};
+
+#endif // PTREPORTOVERLAY_H
