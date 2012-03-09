1c4fb2c23c4235d6ea67884ec2b28464572fd38e
Brother John <Brother John@localhost>
2012-03-09 20:57:54 +0100

    Spot interaction Curve load and save Missing stuff for local spot data class

---
+++ Sources/imagespot/ptSpotInteraction.cpp
@@ -0,0 +1,46 @@
+/*******************************************************************************
+**
+** Photivo
+**
+** Copyright (C) 2012 Bernd Schoeler <brjohn@brother-john.net>
+**
+** This file is part of Photivo.
+**
+** Photivo is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License version 3
+** as published by the Free Software Foundation.
+**
+** Photivo is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Photivo.  If not, see <http://www.gnu.org/licenses/>.
+**
+*******************************************************************************/
+
+#include "ptSpotInteraction.h"
+
+//==============================================================================
+
+ptSpotInteraction::ptSpotInteraction(QGraphicsView *AView)
+  : ptImageInteraction(AView)
+{}
+
+//==============================================================================
+
+void ptSpotInteraction::stop() {
+  emit finished(stSuccess);
+}
+
+//==============================================================================
+
+void ptSpotInteraction::mouseAction(QMouseEvent *AEvent) {
+  if (AEvent->type() == QEvent::MouseButtonPress && AEvent->button() == Qt::LeftButton) {
+    QPointF hClickPos = FView->mapToScene(AEvent->pos());
+    emit clicked(QPoint(qRound(hClickPos.x()), qRound(hClickPos.y()) ) );
+  }
+}
+
+//==============================================================================
