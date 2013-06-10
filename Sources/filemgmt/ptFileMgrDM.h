95e6afa516428120515910c89a57b1723ad8f0aa
Brother John <Brother John@localhost>
2013-06-10 23:02:11 +0200

    * Cleanup for ptFileMgrDM. Also it's not a singleton anymore.

--- Sources/filemgmt/ptFileMgrDM.h
+++ Sources/filemgmt/ptFileMgrDM.h
@@ -2,8 +2,8 @@
 **
 ** Photivo
 **
-** Copyright (C) 2011 Bernd Schoeler <brjohn@brother-john.net>
-** Copyright (C) 2011 Michael Munzert <mail@mm-log.com>
+** Copyright (C) 2011-2013 Bernd Schoeler <brjohn@brother-john.net>
+** Copyright (C) 2011-2013 Michael Munzert <mail@mm-log.com>
 **
 ** This file is part of Photivo.
 **
@@ -24,101 +24,62 @@
 #ifndef PTFILEMGRDM_h
 #define PTFILEMGRDM_h
 
-//==============================================================================
-
+#include "ptGraphicsThumbGroup.h"
+#include "ptSingleDirModel.h"
+#include "ptTagModel.h"
+#include "ptThumbGen.h"
 #include <QObject>
 #include <QPixmap>
 #include <QFileSystemModel>
 #include <QGraphicsItemGroup>
 #include <QList>
 #include <QHash>
-
 #include <wand/magick_wand.h>
-
-#include "ptGraphicsThumbGroup.h"
-#include "ptSingleDirModel.h"
-#include "ptTagModel.h"
-#include "ptThumbGen.h"
-
-//==============================================================================
 
 class ptImage8;
 
-//==============================================================================
-
+//------------------------------------------------------------------------------
 /*!
   \class ptFileMgrDM
 
   \brief Data module for file management.
 
   This data module will handle the thumbnail creation and manage the corresponding
-  memory. It's designed as a singleton.
+  memory.
 */
 class ptFileMgrDM: public QObject {
 Q_OBJECT
 
 public:
-  /*! Get or create the singleton instance of \c ptFileMgrDM */
-  static ptFileMgrDM* GetInstance();
-
-  /*! Destroy the singleton instance of \c ptFileMgrDM */
-  static void DestroyInstance();
-
-//-------------------------------------
-
-  /*! Clear the data cache of \c ptFileMgrDM */
-  void Clear();
-
-  /*! Returns the current folder for thumbnail display. */
-  QString currentDir() { return m_CurrentDir.absolutePath(); }
-
-  /*! Returns a pointer to the model for the folder ListView. */
-  ptSingleDirModel* dirModel() const { return m_DirModel; }
-
-  int focusedThumb() { return m_FocusedThumb; }
-  int focusedThumb(QGraphicsItem* group);
-
-  ptGraphicsThumbGroup* MoveFocus(const int index);
-
-  void populateThumbs(QGraphicsScene* AScene);
-
-  /*! Sets the folder for thumbnail display. Does not trigger the thumbnailer.
-      You probably need this only once to init the folder. */
-  void setCurrentDir(const QString absolutePath) { m_CurrentDir = absolutePath; }
-
-  /*! Sets the directory for thumbnail generation.
-    Returns the total number of applicable entries in that directory.
-    Returns \c -1 and does not set the directory if the thumbnailer is
-    currently running.
-    \param path
-      Sets the directory for thumbnail generation. Must be an absolute path.
-  */
-  int setThumDir(const QString& hAbsolutePath);
-
-  /*! Returns a pointer to the tag model. */
-  ptTagModel* tagModel() { return m_TagModel; }
-
-  /*! Returns a pointer to the list of currently displayed thumbnail images. */
-  QList<ptGraphicsThumbGroup*>* thumbGroupList() { return FThumbGroupList; }
-
-  void connectThumbGen(const QObject* AReceiver, const char* ABroadcastSlot);
-  bool thumbGenRunning() const;
-  void abortThumbGen();
-
-private:
-  static ptFileMgrDM* m_Instance;
-
-  ptFileMgrDM();
-  ptFileMgrDM(const ptFileMgrDM&): QObject() {}
+  ptFileMgrDM(QObject* AParent = nullptr);
   ~ptFileMgrDM();
 
+  void clear();
+
+  QString currentDir();
+  void setCurrentDir(const QString& AAbsolutePath);
+  int setThumDir(const QString& AAbsolutePath);
+  int focusedThumb();
+  int focusedThumb(QGraphicsItem* group);
+  ptGraphicsThumbGroup* moveFocus(const int index);
+
+  void abortThumbGen();
+  void connectThumbGen(const QObject* AReceiver, const char* ABroadcastSlot);
+  void populateThumbs(QGraphicsScene* AScene);
+  QList<ptGraphicsThumbGroup*>* thumbGroupList();
+  bool thumbGenRunning() const;
+
+  ptSingleDirModel* dirModel() const;
+  ptTagModel* tagModel();
+
+private:
   void createThumbGroup(const QFileInfo &AFileInfo, uint AId, QGraphicsScene* AScene);
 
-  int                           m_FocusedThumb;
-  QDir                          m_CurrentDir;
-  ptSingleDirModel*             m_DirModel;
-  bool                          m_IsMyComputer;
-  ptTagModel*                   m_TagModel;
+  int                           FFocusedThumb;
+  QDir                          FCurrentDir;
+  ptSingleDirModel*             FDirModel;
+  bool                          FIsMyComputer;
+  ptTagModel*                   FTagModel;
   std::unique_ptr<ptThumbGen>   FThumbGen;
   QList<ptGraphicsThumbGroup*>* FThumbGroupList;
 
