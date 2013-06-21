312f225d4623f4cdf3d5d254ad92065e23f5b50a
Brother John <Brother John@localhost>
2013-06-21 17:52:45 +0200

    * Implemented thumbnail cache * Corrected folders sorted at the beginning in the thumbnail display * ptImage8: minor cleanup  and added sizeBytes() function

--- Sources/filemgmt/ptThumbCache.h
+++ Sources/filemgmt/ptThumbCache.h
@@ -2,7 +2,7 @@
 **
 ** Photivo
 **
-** Copyright (C) 2013 Michael Munzert <mail@mm-log.com>
+** Copyright (C) 2013 Bernd Schoeler <brjohn@brother-john.net>
 **
 ** This file is part of Photivo.
 **
@@ -18,18 +18,28 @@
 ** You should have received a copy of the GNU General Public License
 ** along with Photivo.  If not, see <http://www.gnu.org/licenses/>.
 **
+*******************************************************************************
+**
+** Shout-outs go out to Tim Day, whose article "LRU cache implementation in C++"
+** <http://timday.bitbucket.org/lru.html> was a great inspiration for this
+** implementation.
+**
 *******************************************************************************/
 
 #ifndef PTTHUMBCACHE_H
 #define PTTHUMBCACHE_H
 
 #include "ptThumbDefines.h"
+#include <QHash>
+#include <QMap>
+#include <QLinkedList>
+#include <QString>
 
-/*! \class ptThumbCache is responsible for caching thumbnail image data. */
+/*! ptThumbCache implements a least-recently-used cache for thumbnail images. */
 class ptThumbCache {
 
 public:
-  explicit ptThumbCache();
+  explicit ptThumbCache(uint AMaxSizeBytes);
   ~ptThumbCache();
 
   void clear();
@@ -37,6 +47,29 @@ public:
   void insert(const TThumbId &AThumbId, TThumbPtr AThumbData);
 
 private:
+  // Types for the cache data structures. For an in-depth discussion of the
+  // cache’s structure see http://timday.bitbucket.org/lru.html
+  typedef QString TThumbKey;
+  typedef QLinkedList<TThumbKey> TAccessTracker;
+
+  // Size of the image is saved independently because it might change between cache insertion
+  // and eviction. However both actions must be performed with the same size to ensure proper
+  // occupancy calculation.
+  struct TThumbValue {
+    TThumbPtr                 Data;
+    uint                      Size;
+    TAccessTracker::iterator  AccessIter;
+  };
+
+  typedef QHash<TThumbKey, TThumbValue> TThumbCache;
+
+  void      evict();
+  TThumbKey makeHashable(const TThumbId& AThumbId) const;
+
+  TAccessTracker  FAccess;
+  TThumbCache     FCache;
+  uint            FCapacity;  // in bytes
+  uint            FOccupancy; // in bytes
 };
 
 #endif // PTTHUMBCACHE_H
